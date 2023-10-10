13ae129c9e615459fd373c04275dece37e24665b
Karim Hasibuan <amfibicoding19@gmail.com>
2023-10-10 14:02:37 +0700

    feat: implement the logic for ascending the car by year.

--- scripts/sortCarByYearAscendingly.js
+++ scripts/sortCarByYearAscendingly.js
@@ -1,13 +1,21 @@
 function sortCarByYearAscendingly(cars) {
-  // Sangat dianjurkan untuk console.log semua hal hehe
-  console.log(cars);
+    // Sangat dianjurkan untuk console.log semua hal hehe
+    console.log(cars);
 
-  // Clone array untuk menghindari side-effect
-  // Apa itu side effect?
-  const result = [...cars];
+    // Clone array untuk menghindari side-effect
+    // Apa itu side effect?
+    const result = [...cars];
 
-  // Tulis code-mu disini
-
-  // Rubah code ini dengan array hasil sorting secara ascending
-  return result;
+    // Tulis code-mu disini
+    for (let i = 0; i < result.length; i++) {
+        for (let j = 0; j < result.length - i - 1; j++) {
+            if (result[j].year > result[j + 1].year) {
+                const temp = result[j];
+                result[j] = result[j + 1];
+                result[j + 1] = temp;
+            }
+        }
+    }
+    // Rubah code ini dengan array hasil sorting secara ascending
+    return result;
 }
