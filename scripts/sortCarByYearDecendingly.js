f839d2e858a1bc7e6c1c23f238578df169364a56
Karim Hasibuan <amfibicoding19@gmail.com>
2023-10-13 20:12:03 +0700

    refactor: implement destructuring of array.

--- scripts/sortCarByYearDecendingly.js
+++ scripts/sortCarByYearDecendingly.js
@@ -10,9 +10,7 @@ function sortCarByYearDescendingly(cars) {
     for (let i = 0; i < result.length; i++) {
         for (let j = 0; j < result.length - 1; j++) {
             if (result[j].year < result[j + 1].year) {
-                const temp = result[j];
-                result[j] = result[j + 1];
-                result[j + 1] = temp;
+                [result[j], result[j + 1]] = [result[j + 1], result[j]];
             }
         }
     }
