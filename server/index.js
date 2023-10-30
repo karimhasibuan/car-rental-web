615b09addfc17e8a17695e0988690ea07844e26a
Karim Hasibuan <amfibicoding19@gmail.com>
2023-10-30 19:24:50 +0700

    fix: change dir name of images.

--- server/index.js
+++ server/index.js
@@ -1,28 +1,28 @@
-const http = require("http");
-const getHtml = require("./helpers/getHtml.js")
-const fs = require("fs");
-const readStaticFiles = require("./helpers/readStaticFiles.js")
+const http = require('http');
+const getHtml = require('./helpers/getHtml.js');
+const fs = require('fs');
+const readStaticFiles = require('./helpers/readStaticFiles.js');
 const PORT = 8000;
-const data = require("./helpers/data.js")
+const data = require('./helpers/data.js');
 const onReq = (req, res) => {
-    const url  = req.url;
-    
-    if(url === "/"){
-        console.log({data})
-        getHtml("index.html", res);
-    }else if(url === "/cars"){
-        getHtml("carList.html", res);
-    }else if(url.match("\.js$")){
-        readStaticFiles(url, res, "text/js", "UTF-8");
-    }else if(url.match("\.css$")){
-        readStaticFiles(url, res, "text/css", "UTF-8");
-    }else if(url.match("\.png$")){
-        readStaticFiles(url, res, "image/png")
+    const url = req.url;
+
+    if (url === '/') {
+        console.log({ data });
+        getHtml('index.html', res);
+    } else if (url === '/cars') {
+        getHtml('carList.html', res);
+    } else if (url.match('.js$')) {
+        readStaticFiles(url, res, 'text/js', 'UTF-8');
+    } else if (url.match('.css$')) {
+        readStaticFiles(url, res, 'text/css', 'UTF-8');
+    } else if (url.match('.jpg$')) {
+        readStaticFiles(url, res, 'images/jpg');
     }
-}
+};
 
 const server = http.createServer(onReq);
 
-server.listen(PORT, "0.0.0.0", ()=> {
-    console.info(`is listening on port ${PORT}`)
+server.listen(PORT, '0.0.0.0', () => {
+    console.info(`is listening on port ${PORT}`);
 });
